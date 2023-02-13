0581377cdd469eed3ba844465c448750ff73a106
ArthurYurkov <77395695+ArthurYurkov@users.noreply.github.com>
2023-02-13 11:24:35 +0200

    first commit

---
+++ contacts.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const fs = require("fs/promises");
+
+const contactsPath = path.join(__dirname, "bd/contacts.json");
+
+async function listContacts() {
+  const list = await fs.readFile(contactsPath, "utf-8");
+  const allList = JSON.parse(list);
+  return allList;
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const result = contacts.find((contact) => contact.id === contactId);
+  return result || null;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return result;
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+};
